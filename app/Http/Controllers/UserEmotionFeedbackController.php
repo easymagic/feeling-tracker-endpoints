ec5d7594476d7dc426bc760e484df3013efed903
easymagic <nnamware@yahoo.com>
2021-02-13 13:36:01 +0100

    np

---
+++ app/Http/Controllers/UserEmotionFeedbackController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserEmotionFeedback;
+use Illuminate\Http\Request;
+
+class UserEmotionFeedbackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UserEmotionFeedback  $userEmotionFeedback
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserEmotionFeedback $userEmotionFeedback)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UserEmotionFeedback  $userEmotionFeedback
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserEmotionFeedback $userEmotionFeedback)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UserEmotionFeedback  $userEmotionFeedback
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserEmotionFeedback $userEmotionFeedback)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UserEmotionFeedback  $userEmotionFeedback
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserEmotionFeedback $userEmotionFeedback)
+    {
+        //
+    }
+}
