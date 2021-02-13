f3c15bbe7d345322a47602de8d6c4735ca2a5e7c
easymagic <nnamware@yahoo.com>
2021-02-13 18:00:25 +0100

    np

--- app/Models/UserEmotionFeedback.php
+++ app/Models/UserEmotionFeedback.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class UserEmotionFeedback extends Model
 {
     use HasFactory;
+
+
+    static function factoryCreate($data){
+        return (new self)->createFeedback($data);
+    }
+
+    function userIsValid($user_id){
+        return (new User)->newQuery()->where('id',$user_id)->exists();
+    }
+
+    function scaleIsValid($scale_id){
+        return (new Scale)->newQuery()->where('scale_id',$scale_id)->exists();
+    }
+
+    function createFeedback($data){
+
+        if (!$this->userIsValid($data['user_id'])){
+            return [
+                'message'=>'Invalid user!',
+                'error'=>true
+            ];
+        }
+
+
+        $this->user_id = $data['user_id'];
+        $this->scale_id = $data['scale_id'];
+        $this->feedback = $data['feedback'];
+
+        $this->save();
+
+    }
+
+
 }
